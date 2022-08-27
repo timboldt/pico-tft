91b09d9971fc386c81f8c47af36a149354d3bd58
Tim Boldt <tim.boldt@gmail.com>
2022-08-27 08:48:36 -0700

    Refactor source code dir (add src)

--- flash.sh
+++ flash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 openocd -f interface/jlink.cfg -c "transport select swd" -c "adapter_khz 6000" -f target/rp2040.cfg  \
-    -c "program build/hello_world.elf reset exit"
+    -c "program build/src/picotft.elf reset exit"
