0917daee89d774d3836fa59795a74c607fa51a30
Tim Boldt <tim.boldt@gmail.com>
2022-09-11 21:00:51 -0700

    Basic implementation of WiFi connectivity

--- src/main.c
+++ src/main.c
@@ -14,59 +14,62 @@
 
 #include <stdio.h>
 
-//#include "drivers/ds3231.h"
-#include "hardware/i2c.h"
-//#include "pico/binary_info.h"
+// #include "drivers/ds3231.h"
+// #include "hardware/i2c.h"
 #include "lvgl.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include "secrets.h"
 
-#define FEATHER_I2C_BUS i2c1
-#define FEATHER_I2C_SDA_PIN 2
-#define FEATHER_I2C_SCL_PIN 3
+// #define FEATHER_I2C_BUS i2c1
+// #define FEATHER_I2C_SDA_PIN 2
+// #define FEATHER_I2C_SCL_PIN 3
 
-void init_i2c_bus(void) {
-    const uint baudrate_khz = 100;
-    i2c_init(FEATHER_I2C_BUS, baudrate_khz * 1000);
-    gpio_set_function(FEATHER_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(FEATHER_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(FEATHER_I2C_SDA_PIN);
-    gpio_pull_up(FEATHER_I2C_SCL_PIN);
-}
+// void init_i2c_bus(void) {
+//     const uint baudrate_khz = 100;
+//     i2c_init(FEATHER_I2C_BUS, baudrate_khz * 1000);
+//     gpio_set_function(FEATHER_I2C_SDA_PIN, GPIO_FUNC_I2C);
+//     gpio_set_function(FEATHER_I2C_SCL_PIN, GPIO_FUNC_I2C);
+//     gpio_pull_up(FEATHER_I2C_SDA_PIN);
+//     gpio_pull_up(FEATHER_I2C_SCL_PIN);
+// }
 
-int main() {
-    stdio_init_all();
-    init_i2c_bus();
-    lv_init();
-
-    sleep_ms(5000);
-
+void init_wifi(void) {
     printf("cyw43_arch_init_with_country()...\n");
     if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
         printf("cyw43_arch_init_with_country() failed.\n");
-        return 1;
+        return;
     }
 
     printf("cyw43_arch_enable_sta_mode()...\n");
     cyw43_arch_enable_sta_mode();
 
-    printf("cyw43_arch_wifi_connect_timeout_ms()...\n");
-    int err = cyw43_arch_wifi_connect_timeout_ms(
-        WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000);
+    printf("cyw43_arch_wifi_connect_async()...\n");
+    int err = cyw43_arch_wifi_connect_async(
+        WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK);
     if (err) {
-        printf("cyw43_arch_wifi_connect_timeout_ms() failed: %d\n", err);
-        return 1;
+        printf("cyw43_arch_wifi_connect_async() failed: %d\n", err);
+        return;
     }
-    printf("WiFi connected.\n");
+}
+
+int main() {
+    stdio_init_all();
+    // init_i2c_bus();
+    lv_init();
+
+    sleep_ms(5000);
+    init_wifi();
 
     while (true) {
+        cyw43_arch_poll();
+
         // struct tm now = {};
         // ds3231_fetch_time(FEATHER_I2C_BUS, &now);
         // printf("%s\n", asctime(&now));
 
-        printf("Tick!\n");
-        sleep_ms(1000);
+        printf("WiFi status: %d\n", cyw43_wifi_link_status(&cyw43_state, CYW43_ITF_STA));
+        sleep_ms(100);
 
         // These don't belong here. (Part of it needs to go in a timer
         // callback.) lv_timer_handler(); lv_tick_inc(10);
